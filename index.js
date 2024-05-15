bc926135e49a20f9c0836efbb1c1f1f46bc32cf7
t0rR3nte <33793507+t0rR3nte@users.noreply.github.com>
2024-05-14 23:44:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,7 @@ const aceptarDatosCheckbox = document.getElementById('aceptarDatos');
 
 btnAceptar.addEventListener('click', () => {
     if (aceptarDatosCheckbox.checked) {
-        const aceptaDatos = aceptarDatosCheckbox.checked;
-
-        if (aceptaDatos == true) {
-                alert('Su aceptación de datos ha sido registrada correctamente.');
-            } else {
-                alert('Error al registrar su aceptación de datos.');
-            }
-        })
-        .catch(error => {
-            console.error(error);
-            alert('Error en la comunicación con el servidor.');
-        });
+        alert('Su aceptación de datos ha sido registrada correctamente.');
     } else {
         alert('Debe aceptar la política de tratamiento de datos para continuar.');
     }
